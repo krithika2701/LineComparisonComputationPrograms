582a7390238986b989400cc6e822f0b6aae129e8
krithika2701 <shirisharudrarapu21@gmail.com>
2023-03-21 22:30:43 +0530

    finding the legth of the line in cartesian system

--- LineComparisonPrograms.java
+++ LineComparisonPrograms.java
@@ -1,10 +1,23 @@
 package com.bridgelabz.linecomparisoncomputationprograms;
 
-/* Welcome message for line comparison computation programs */
+import java.util.Scanner;
+
+//* Welcome message for line comparison computation programs *//
 
 public class LineComparisonPrograms {
 	public static void main(String[] args) {
 		System.out.println("Welcome message for line comparison computation programs");
+		Scanner sc = new Scanner(System.in);
+		double x1, y1, x2, y2, LengthOfLine;
+		System.out.println("Enter the point one (x1,y1) ");
+		x1 = sc.nextDouble();
+		y1 = sc.nextDouble();
+		System.out.println("Enter the point two (x2,y2)");
+		x2 = sc.nextDouble();
+		y2 = sc.nextDouble();
+		LengthOfLine = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+
+		System.out.println("Length of a line =" + LengthOfLine);
 
 	}
 
