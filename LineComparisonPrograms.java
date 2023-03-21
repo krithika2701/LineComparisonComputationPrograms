df7c9a3b7de45d15561d903723b4f32b75e11078
krithika2701 <shirisharudrarapu21@gmail.com>
2023-03-21 12:02:16 +0530

    Add:Welcome massage for line comparison programs

---
+++ LineComparisonPrograms.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.linecomparisoncomputationprograms;
+
+/* Welcome message for line comparison computation programs */
+
+public class LineComparisonPrograms {
+	public static void main(String[] args) {
+		System.out.println("Welcome message for line comparison computation programs");
+
+	}
+
+}
